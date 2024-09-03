e29a78ce28bf6a458d8218cd7209b99305d7d316
ajack3 <ajack3@CSELAB6-059.unl.edu>
2024-09-03 11:33:30 -0500

    Added name and major

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Alexander Stadler");
 		System.out.println("Computer Science");
+		System.out.println("Aidan Jack");
+		System.out.println("Computer Science");
 	}
 }
